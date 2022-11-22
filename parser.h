8924b68846f5276d9b41eacca8496bdf37577b21
Maroš Synák <xsynak03@stud.fit.vutbr.cz>
2022-11-22 19:21:59 +0100

    Added function lightly tested

--- parser.h
+++ parser.h
@@ -22,14 +22,16 @@ token_ptr assert_next_token_get(item_ptr *stack, token_type_t token_type);
 
 // -- rule functions
 
-void rule_prog(item_ptr *in_stack, tree_node_ptr tree);
-void rule_prog_end(item_ptr *in_stack, tree_node_ptr tree);
-void rule_statement(item_ptr *in_stack, tree_node_ptr tree);
-void rule_statement_list(item_ptr *in_stack, tree_node_ptr tree);
+void rule_prog(item_ptr *in_stack, table_node_ptr *tree);
+void rule_prog_end(item_ptr *in_stack, table_node_ptr *tree);
+void rule_statement(item_ptr *in_stack, table_node_ptr *tree);
+void rule_statement_list(item_ptr *in_stack, table_node_ptr *tree);
+
+void rule_argument_list_typ(item_ptr *in_stack, table_node_ptr *tree);
+void rule_argument_next_typ(item_ptr *in_stack, table_node_ptr *tree);
 
 // TODO: Redefine int to a expression result union type
 int parse_expression(item_ptr *in_stack);
 
-tree_node_ptr parse(array_ptr tokens);
-
+table_node_ptr parse(array_ptr tokens);
 #endif
