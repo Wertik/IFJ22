4eb44c3475d07a835a297dd89284a145478404ce
Maroš Synák <xsynak03@stud.fit.vutbr.cz>
2022-11-25 13:42:52 +0100

    is this the right way to do it? using tokens instead of sepparate alphabet

--- precedence_expression.c
+++ precedence_expression.c
@@ -42,23 +42,69 @@ void perform_reduction(item_ptr *push_down_stack, table_node_ptr *tree){
     fprintf(stderr,"NOT POSSIBLE TO REDUCE");
 
 }
+
+void perform_addition(item_ptr *push_down_stack, table_node_ptr *tree , item_ptr *in_stack){
+    token_ptr next = get_next_token(in_stack);
+    symbol_ptr symbol = create_terminal(next);
+    stack_push(push_down_stack, symbol);
+
+}
+
 int expression(item_ptr *in_stack, table_node_ptr *tree){
     
     item_ptr push_down_stack = stack_init();
+
     // represents dollar
     token_value_t value;
     token_ptr dollar = token_create(TOKEN_OPENING_TAG, NONE, value);
     symbol_ptr symbol = create_terminal(dollar);
+
     //dollar = create_terminal(dollar);
     DEBUG_OUT("$");
     push_down_stack = stack_push(in_stack, symbol);
-    token_ptr next = get_next_token(in_stack);
-    switch (next->type)
+    token_ptr next_in_push = peek_top(push_down_stack);
+    switch (next_in_push->type)
     {
-    case /* constant-expression */:
+    case TOKEN_OPENING_TAG :
         /* code */
         break;
-    
+    case TOKEN_PLUS:
+    case TOKEN_MINUS:
+    case TOKEN_DOT:
+        /* code */
+        break;
+    case TOKEN_DIVIDE:
+    case TOKEN_MULTIPLE:
+        /* code */
+        break;
+    case TOKEN_L_PAREN:
+        /* code */
+        break;
+    case TOKEN_R_PAREN:
+        /* code */
+        break;
+    case TOKEN_LESS:
+    case TOKEN_MORE:
+    case TOKEN_LESS_EQUAL:
+    case TOKEN_MORE_EQUAL:
+        /* code */
+        break;
+    case TOKEN_EQUAL:
+    case TOKEN_NOT_EQUAL:
+        /* code */
+        break;
+    case TOKEN_STRING_LIT:
+        /* code */
+        break;
+    case TOKEN_CONST_INT:
+        /* code */
+        break;
+    case TOKEN_CONST_DOUBLE:
+        /* code */
+        break;
+    case TOKEN_CONST_EXP:
+        /* code */
+        break;
     default:
         break;
     }
