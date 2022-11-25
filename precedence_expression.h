593eb7b862f3dad205fa41d5f3a27598994adf1a
Maroš Synák <xsynak03@stud.fit.vutbr.cz>
2022-11-25 15:07:43 +0100

    added get_frist_non_E

--- precedence_expression.h
+++ precedence_expression.h
@@ -5,9 +5,11 @@
 #include "symtable.h"
 #include "stack.h"
 
-
+int get_pos_in_t(token_ptr TOKEN);
 void perform_addition(item_ptr *push_down_stack, table_node_ptr *tree , item_ptr *in_stack);
 void perform_reduction(item_ptr *push_down_stack, table_node_ptr *tree);
+
+token_ptr get_first_non_E(item_ptr *stack);
 
 int expression(item_ptr *push_down_stack, table_node_ptr *tree);
 #endif
