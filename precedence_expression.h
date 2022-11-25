6ced03f600ce8ee115baf400c03cc8e3f9a9622f
Maroš Synák <xsynak03@stud.fit.vutbr.cz>
2022-11-25 13:43:40 +0100

    h file

--- precedence_expression.h
+++ precedence_expression.h
@@ -5,5 +5,9 @@
 #include "symtable.h"
 #include "stack.h"
 
+
+void perform_addition(item_ptr *push_down_stack, table_node_ptr *tree , item_ptr *in_stack);
+void perform_reduction(item_ptr *push_down_stack, table_node_ptr *tree);
+
 int expression(item_ptr *push_down_stack, table_node_ptr *tree);
 #endif
